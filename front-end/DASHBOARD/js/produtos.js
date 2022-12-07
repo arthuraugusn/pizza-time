4b18f0253267493b9202c9139876849745974fa2
Arthur Augusto <98403987+arthuraugusn@users.noreply.github.com>
2022-12-07 14:39:00 -0300

    cmmt

--- front-end/DASHBOARD/js/produtos.js
+++ front-end/DASHBOARD/js/produtos.js
@@ -15,14 +15,29 @@ const bebidas = await getAllBebidas()
 
 const consumirArray = function (array) {
 
+    const a = document.createElement('a')
+    a.classList.add('link_forms')
+    a.href = "../index/forms.html"
+
+    if(array.id_pizza){
+        a.id = array.id_pizza
+    }else if(array.id_bebida){
+        a.id = array.id_bebida
+    }
+
     const divPizza  =  document.createElement('div')
     divPizza.classList.add('caixa_produto')
 
     const nome = document.createElement('span')
+    nome.classList.add('nome')
     if(array.nome_pizza){
-        nome.textContent = array.nome_pizza
+        if(array.tamanho_pizza){
+            nome.textContent = `1 ${array.nome_pizza}, ${array.tamanho_pizza}`
+        }
     }else if(array.nome_bebida){
-        nome.textContent = array.nome_bebida
+        if(array.tamanho_bebida){
+            nome.textContent = `1 ${array.nome_bebida}, ${array.tamanho_bebida}`
+        }
     }
 
     const img = document.createElement('img')
@@ -30,19 +45,15 @@ const consumirArray = function (array) {
 
     const preco = document.createElement('span')
     preco.textContent = `R$ ${array.preco}`
+    preco.classList.add('preco')
 
-    const tamanho = document.createElement('span')
-    if(array.tamanho_pizza){
-        tamanho.textContent = `Tamanho: ${array.tamanho_pizza}`
-    }else if (array.tamanho_bebida){
-        tamanho.textContent = `Tamanho: ${array.tamanho_bebida}`
-    }
     divPizza.appendChild(img)
     divPizza.appendChild(preco)
     divPizza.appendChild(nome)
-    divPizza.appendChild(tamanho)
 
-    return divPizza
+    a.appendChild(divPizza)
+
+    return a
 }
 
 const selectCategoria = function(array){
@@ -63,33 +74,8 @@ const selectCategoria = function(array){
     return selectCategoria
 }
 
-const selectTipoProduto = function(array){
-
-    const optionPizzaBebida = document.createElement('option')
-
-    if(array.tipo_pizza){
-        optionPizzaBebida.textContent = array.tipo_pizza
-    }else if(array.tipo_bebida){
-        optionPizzaBebida.textContent = array.tipo_bebida
-    }
-
-    return optionPizzaBebida
-}
-
 selectCategoria(pizzas)
 selectCategoria(bebidas)
-
-const teste = async () => {
-    const galeriaPizzas = document.getElementById('produto')
-    const tagPizzas = pizzas.pizzas.map(selectTipoProduto)
-    galeriaPizzas.replaceChildren(...tagPizzas)
-
-    const galeriaBebidas = document.getElementById('produto')
-    const tagBebidas = bebidas.bebidas.map(selectTipoProduto)
-    galeriaBebidas.replaceChildren(...tagBebidas)
-}
-
-teste()
 
 const pesquisarPizzas = async () => {
     const galeriaPizzas = document.querySelector('.pizzas')
@@ -103,27 +89,15 @@ const pesquisarBebidas = async () => {
     galeriaBebidas.replaceChildren(...tagBebidas)
 }
 
-const pesquisar = async () => {
-    const galeriaPizzas = document.querySelector('.pizzas')
-    const tagPizzas = pizzas.pizzas.map(consumirArray)
-    galeriaPizzas.replaceChildren(...tagPizzas)
-
-    const galeriaBebidas = document.querySelector('.bebidas')
-    const tagBebidas = bebidas.bebidas.map(consumirArray)
-    galeriaBebidas.replaceChildren(...tagBebidas)
-}
-
-if(document.getElementById('categoria').value.toLowerCase() == 'pizzas' || document.getElementById('categoria').value.toLowerCase() == 'bebidas'){
-    pesquisar()
+if(document.getElementById('categoria').value.toLowerCase() == 'pizzas'){
+    pesquisarPizzas()
 }
 
 document.getElementById('categoria').addEventListener('change', async (event)=>{
+    if(event.target.value.toLowerCase() == 'bebidas'){
+        pesquisarBebidas()
+    }
     if(event.target.value.toLowerCase() == 'pizzas'){
-        await pesquisarPizzas()
-        if(document.querySelector('.bebidas').classList.contains('bebidas')){
-            document.querySelector('.bebidas').innerHTML = ''
-        }
-    }else if(event.target.value.toLowerCase() == 'bebidas'){
-        await pesquisarBebidas()
+        pesquisarPizzas()
     }
 })
